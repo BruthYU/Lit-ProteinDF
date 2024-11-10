4d4ace1cb3949bf069c03e9342eea7d0c270cdd5
BruthYU <462633494@qq.com>
2024-11-10 10:49:32 +0800

    #TODO debug foldflow datamofule

--- lightning/data/foldflow/dataloader.py
+++ lightning/data/foldflow/dataloader.py
@@ -6,12 +6,14 @@ import os
 
 import numpy as np
 import torch
+import math
 from torch.utils import data
 import collections
-
+import torch.distributed as dist
 from preprocess.tools import residue_constants
 from evaluate.openfold.utils import rigid_utils
 from typing import Dict, List, Tuple, Union, Any
+from typing import Any, Optional
 from Bio import PDB
 from Bio.PDB.Chain import Chain
 import dataclasses
@@ -229,3 +231,155 @@ def create_data_loader(
         if num_workers != 0
         else None,  # TODO Try without. Doesn't seem to matter
     )
+
+
+class OldDistributedTrainSampler(data.Sampler):
+    r"""Sampler that restricts data loading to a subset of the dataset.
+
+    modified from torch.utils.data.distributed import DistributedSampler
+
+    .. note::
+        Dataset is assumed to be of constant size and that any instance of it always
+        returns the same elements in the same order.
+
+    Args:
+        dataset: Dataset used for sampling.
+        num_replicas (int, optional): Number of processes participating in
+            distributed training. By default, :attr:`world_size` is retrieved from the
+            current distributed group.
+        rank (int, optional): Rank of the current process within :attr:`num_replicas`.
+            By default, :attr:`rank` is retrieved from the current distributed
+            group.
+        shuffle (bool, optional): If ``True`` (default), sampler will shuffle the
+            indices.
+        seed (int, optional): random seed used to shuffle the sampler if
+            :attr:`shuffle=True`. This number should be identical across all
+            processes in the distributed group. Default: ``0``.
+        drop_last (bool, optional): if ``True``, then the sampler will drop the
+            tail of the data to make it evenly divisible across the number of
+            replicas. If ``False``, the sampler will add extra indices to make
+            the data evenly divisible across the replicas. Default: ``False``.
+
+    .. warning::
+        In distributed mode, calling the :meth:`set_epoch` method at
+        the beginning of each epoch **before** creating the :class:`DataLoader` iterator
+        is necessary to make shuffling work properly across multiple epochs. Otherwise,
+        the same ordering will be always used.
+
+    Example::
+
+        >>> # xdoctest: +SKIP
+        >>> sampler = DistributedSampler(dataset) if is_distributed else None
+        >>> loader = DataLoader(dataset, shuffle=(sampler is None),
+        ...                     sampler=sampler)
+        >>> for epoch in range(start_epoch, n_epochs):
+        ...     if is_distributed:
+        ...         sampler.set_epoch(epoch)
+        ...     train(loader)
+    """
+
+    def __init__(
+        self,
+        *,
+        data_conf,
+        dataset,
+        batch_size,
+        num_replicas: Optional[int] = None,
+        rank: Optional[int] = None,
+        shuffle: bool = True,
+        seed: int = 0,
+        drop_last: bool = False,
+    ) -> None:
+        if num_replicas is None:
+            if not dist.is_available():
+                raise RuntimeError("Requires distributed package to be available")
+            num_replicas = dist.get_world_size()
+        if rank is None:
+            if not dist.is_available():
+                raise RuntimeError("Requires distributed package to be available")
+            rank = dist.get_rank()
+        if rank >= num_replicas or rank < 0:
+            raise ValueError(
+                "Invalid rank {}, rank should be in the interval"
+                " [0, {}]".format(rank, num_replicas - 1)
+            )
+        self._data_conf = data_conf
+        self._dataset = dataset
+        self._data_csv = self._dataset.csv
+        self._dataset_indices = list(range(len(self._data_csv)))
+        self._data_csv["index"] = self._dataset_indices
+        # _repeated_size is the size of the dataset multiply by batch size
+        self._repeated_size = batch_size * len(self._data_csv)
+        self._batch_size = batch_size
+        self.num_replicas = num_replicas
+        self.rank = rank
+        self.epoch = 0
+        self.drop_last = drop_last
+        # If the dataset length is evenly divisible by # of replicas, then there
+        # is no need to drop any data, since the dataset will be split equally.
+        if self.drop_last and self._repeated_size % self.num_replicas != 0:  # type: ignore[arg-type]
+            # Split to nearest available length that is evenly divisible.
+            # This is to ensure each rank receives the same amount of data when
+            # using this Sampler.
+            self.num_samples = math.ceil(
+                (self._repeated_size - self.num_replicas) / self.num_replicas  # type: ignore[arg-type]
+            )
+        else:
+            self.num_samples = math.ceil(self._repeated_size / self.num_replicas)  # type: ignore[arg-type]
+        self.total_size = self.num_samples * self.num_replicas
+        self.shuffle = shuffle
+        self.seed = seed
+
+    def __iter__(self):
+        if self.shuffle:
+            # deterministically shuffle based on epoch and seed
+            g = torch.Generator()
+            g.manual_seed(self.seed + self.epoch)
+            indices = torch.randperm(len(self._data_csv), generator=g).tolist()  # type: ignore[arg-type]
+        else:
+            indices = list(range(len(self._data_csv)))  # type: ignore[arg-type]
+
+        # indices is expanded by self._batch_size times
+        indices = np.repeat(indices, self._batch_size)
+
+        if not self.drop_last:
+            # add extra samples to make it evenly divisible
+            padding_size = self.total_size - len(indices)
+            if padding_size <= len(indices):
+                indices = np.concatenate((indices, indices[:padding_size]), axis=0)
+            else:
+                indices = np.concatenate(
+                    (
+                        indices,
+                        np.repeat(indices, math.ceil(padding_size / len(indices)))[
+                            :padding_size
+                        ],
+                    ),
+                    axis=0,
+                )
+
+        else:
+            # remove tail of data to make it evenly divisible.
+            indices = indices[: self.total_size]
+        assert len(indices) == self.total_size
+
+        # subsample
+        indices = indices[self.rank : self.total_size : self.num_replicas]
+
+        assert len(indices) == self.num_samples
+
+        return iter(indices)
+
+    def __len__(self) -> int:
+        return self.num_samples
+
+    def add_epoch(self) -> None:
+        r"""
+        Sets the epoch for this sampler. When :attr:`shuffle=True`, this ensures all replicas
+        use a different random ordering for each epoch. Otherwise, the next iteration of this
+        sampler will yield the same ordering.
+
+        Args:
+            epoch (int): Epoch number.
+        """
+        self.epoch = self.epoch + 1
---
+++ lightning/data/foldflow/lightning_datamodule.py
@@ -0,0 +1,100 @@
+import inspect
+import importlib
+import pytorch_lightning as pl
+import random
+import torch
+from torch.utils.data import DataLoader
+import logging
+
+LOG = logging.getLogger(__name__)
+
+
+# TODO Wrap LigntningDataModule into different methods
+
+class foldflow_Lightning_Datamodule(pl.LightningDataModule):
+    def __init__(self, conf):
+        super().__init__()
+        self.save_hyperparameters()
+        self.data_conf = conf.dataset
+        self.exp_conf = conf.experiment
+        self.fm_conf = conf.flow_matcher
+        self.method_name = conf.method_name
+        self.data_module = self.init_data_module(self.method_name)
+        # import utils for to create dataloader
+        self.dataloader = importlib.import_module(f'lightning.data.{self.method_name}.dataloader')
+        self.device = 'cuda:0'
+
+    def setup(self, stage=None):
+        # Assign train/val datasets for use in dataloaders
+        if stage == 'fit' or stage is None:
+            '''Train Dataset'''
+            self.trainset = self.instancialize_module(module=self.data_module, data_conf=self.data_conf, fm_conf=self.fm_conf,
+                                                      is_training=True, is_OT=self.fm_conf.ot_plan, ot_fn=self.fm_conf.ot_fn, reg=self.fm_conf.reg)
+
+            '''Valid Dataset'''
+            self.valset = self.instancialize_module(module=self.data_module, data_conf=self.data_conf, fm_conf=self.fm_conf,
+                                                      is_training=False, is_OT=self.fm_conf.ot_plan, ot_fn=self.fm_conf.ot_fn, reg=self.fm_conf.reg)
+
+
+
+    def train_dataloader(self):
+        # Create Sampler on pre-defined mode
+        num_workers = self.exp_conf.num_loader_workers
+
+        # train_sampler = self.dataloader.TrainSampler(
+        #     data_conf=self.data_conf,
+        #     dataset=self.trainset,
+        #     batch_size=self.exp_conf.batch_size,
+        #     sample_mode=self.exp_conf.sample_mode,
+        # )
+        '''
+        Distributed Sampler
+        '''
+        train_sampler = self.dataloader.OldDistributedTrainSampler(
+                data_conf=self.data_conf,
+                dataset=self.trainset,
+                batch_size=self.exp_conf.batch_size,
+                sample_mode=self.exp_conf.sample_mode,
+                max_squared_res=self.exp_conf.max_squared_res,
+                num_gpus=self.exp_conf.num_gpus,  # TODO fix arg based on actual fabric
+            )
+
+        train_loader = self.dataloader.create_data_loader(
+            self.trainset,
+            sampler=train_sampler,
+            np_collate=False,
+            length_batch=True,
+            batch_size=self.exp_conf.batch_size // train_sampler.num_replicas,
+            shuffle=False,
+            num_workers=num_workers,
+            drop_last=False,
+            max_squared_res=self.exp_conf.max_squared_res,
+        )
+        return train_loader
+
+    def val_dataloader(self):
+        valid_sampler = None
+        valid_loader = self.dataloader.create_data_loader(
+            self.valset,
+            sampler=valid_sampler,
+            np_collate=False,
+            length_batch=False,
+            batch_size=self.exp_conf.eval_batch_size,
+            shuffle=False,
+            num_workers=0,
+            drop_last=False,
+        )
+        return valid_loader
+
+    def instancialize_module(self, module, **other_args):
+        class_args = list(inspect.signature(module.__init__).parameters)[1:]
+        inkeys = other_args.keys()
+        args1 = {}
+        for arg in class_args:
+            if arg in inkeys:
+                args1[arg] = other_args[arg]
+        args1.update(other_args)
+        return module(**args1)
+
+    def init_data_module(self, name, **other_args):
+        return getattr(importlib.import_module(f'data.{name}.dataset'), f'{name}_Dataset')
